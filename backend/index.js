db65af0148d71fe9c78bdc56434c2a98d5d81b7a
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-10-15 14:06:21 +0100

    feat(auth): add basic auth routes and controller placeholders (signup, login, logout)

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import { connectDB } from './db/connectDB.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   connectDB();
