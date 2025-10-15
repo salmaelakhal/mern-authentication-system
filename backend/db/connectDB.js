42b679e43e5e493cec12812686fad52b29bcb1be
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-10-15 13:46:14 +0100

    feat(db): add MongoDB connection setup with mongoose and .env config

---
+++ backend/db/connectDB.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+
+
+export const connectDB = async () => {
+  // Database connection logic here
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI)
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`)
+    process.exit(1)
+  }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,17 @@
-// const express = require('express');
 import express from 'express';
+import dotenv from 'dotenv'
+import { connectDB } from './db/connectDB.js';
+
+dotenv.config();
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+  connectDB();
+  console.log('Server is running on port 3000');
+});
 
