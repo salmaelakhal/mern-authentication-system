c1e7d57560a99509480ad9d7e6d67cb1b8fc4505
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-10-15 19:07:11 +0100

    feat(auth): add signup controller with JWT token stored in secure HTTP-only cookie

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,55 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+
+
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required");
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+
+    if (userAlreadyExists) {
+      return res.status(400).json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const verificationToken = Math.floor(100000 + Math.random() * 900000).toString(); // Generates a 6-digit code
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+        verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,// 24 hours from now
+    });
+
+    await user.save();
+
+    // jwt token
+    generateTokenAndSetCookie(res, user._id);
+
+    res.status(201).json({ success: true, message: "User registered successfully", user: {
+        ...user._doc, password: undefined,
+    }});
+
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message } );
+  }
+};
+
+
+
+//////////////////////////////////////////////////////////////////////
+export const login = async (req, res) => {
+  res.send("Login route");
+};
+
+//////////////////////////////////////////////////////////////////////
+export const logout = async (req, res) => {
+  res.send("Logout route");
+};
--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv'
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use(express.json()); // allows us to parse incoming requests: req.body
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
 
 
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${PORT}`);
 });
 
