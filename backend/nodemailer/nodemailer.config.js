ad27ae7c777bb421742f2cac576893e4b32ddb80
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-10-24 15:00:52 +0100

    Switch email service from Gmail to Brevo SMTP

--- backend/nodemailer/nodemailer.config.js
+++ backend/nodemailer/nodemailer.config.js
@@ -4,24 +4,26 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail", // Gmail comme service
+  host: process.env.MAIL_HOST, // smtp-relay.brevo.com
+  port: process.env.MAIL_PORT || 587,
+  secure: false, // false pour le port 587
   auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS,
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
 });
 
 export const sendEmail = async (to, subject, html) => {
   try {
     const info = await transporter.sendMail({
-      from: `"Auth App" <${process.env.GMAIL_USER}>`,
+      from: `"Auth App" <${process.env.MAIL_USER}>`,
       to,
       subject,
       html,
     });
-    console.log("Email envoyé :", info.messageId);
+    console.log("✅ Email envoyé :", info.messageId);
   } catch (error) {
-    console.error("Erreur envoi email :", error);
+    console.error("❌ Erreur envoi email :", error);
     throw error;
   }
 };
